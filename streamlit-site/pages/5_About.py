d6da6dd3c6cf43d7c70633034bd15b1ac17a3786
Stephen-O-Donovan <booklad@gmail.com>
2023-01-24 10:20:59 +0000

    expanded about

--- streamlit-site/pages/5_About.py
+++ streamlit-site/pages/5_About.py
@@ -6,3 +6,23 @@ st.set_page_config(
 )
 
 st.write("About me")
+
+st.markdown('''
+Born, raised and still hanging around in Cork, Ireland, I've been a lover of all things techie and
+geeky since I can remember. I have a degree in Computer Science and a masters in Data Science,
+several years of experience as a software developer and am always looking for new things to learn
+and try out.
+''')
+
+st.write("About this site")
+
+st.markdown(''' 
+The original intent of this website was to demonstrate the results of my master's thesis:
+"Machine Learning for the use of careless response detection in survey data"
+but it has expanded somewhat. In particular I was jsut going to create a basic website deployed with AWS 
+but the more I learned about the possibilities it offered the more I wanted to try out.
+The various techs used are listed in more details in the "How this site was built section". 
+It has been a load of fun and a great learning experience navigating not just the possibilites
+on offer and how to adapt them to what I wanted, but also figuring out the inevitable 
+problems and how to work around them.
+''')
