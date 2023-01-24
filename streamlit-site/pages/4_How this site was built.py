574ae900c34f1822ff4a0d5e07d8fc8f939abb74
Stephen-O-Donovan <booklad@gmail.com>
2023-01-24 10:29:09 +0000

    expand build page

--- streamlit-site/pages/4_How this site was built.py
+++ streamlit-site/pages/4_How this site was built.py
@@ -6,3 +6,24 @@ st.set_page_config(
 )
 
 st.write("How this site was built")
+
+with st.expander("A (growing) list of tech used"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.header('AWS')
+    st.subheader('Fargate')
+    st.subheader('ECR')
+    st.subheader('S3')
+    st.subheader('SAM')
+    st.subheader('Lambda')
+    st.subheader('Lambda Layers')
+    st.subheader('API Gateway')
+    st.subheader('Secret Manager')
+    st.subheader('AWS CLI')
+    st.header('Local Dev')
+    st.subheader('Git')
+    st.subheader('Docker')
+    st.subheader('Streamlit')
