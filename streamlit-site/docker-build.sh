8b532dda8f1cfab7254beafe60944b9f3fd55a6d
Stephen-O-Donovan <booklad@gmail.com>
2023-01-27 12:37:41 +0000

    changed docker, add delete image script

--- streamlit-site/docker-build.sh
+++ streamlit-site/docker-build.sh
@@ -1,2 +1,10 @@
-docker build --progress=plain -t careless-ecr .
-docker run -it --rm -p '8501:8501' careless-ecr 
+
+
+# # build Docker image in current directory
+# docker build -t 'latest' .
+# # Run docker image with port 8501 and volumes
+# docker run -it --rm --port '8501:8501' -v '$(pwd)/data:/usr/scr/app/data:delegated' -v '$(pwd)/project:/usr/scr/app/project:delegated' latest
+
+docker-compose up -d
+# docker build --progress=plain -t careless-ecr .
+# docker run -it --rm -p '8501:8501' careless-ecr 
